c9643b7e680d7818e2e1389eae695d126e713ffc
Edvini-Code <121399785+Edvini-Code@users.noreply.github.com>
2022-12-25 19:55:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+/*==============SHOW MENU================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/*=====MENU SHOW======*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =======*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*============ REMOVE MENU MOBILE ========*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('.nav-menu')
+    //When we click on each nav__link, we remove the show menu//
+    navMenu,classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction ))
+
+/*================ CHANGE BACKGROUND HEADER================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    //When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window/addEventListener('scroll', scrollHeader)
+
+/*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+       
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            spaceBetween: 48,
+        },
+    },
+});
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+/* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l/addEventListener('click activeFeatured'))
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('id')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offserHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+              
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+        
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active-link')
+
+        }
+    })
+
+
+    
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+    
+})
+
+sr.reveal('.home__title')
+sr.reveal('.home__subtitle', {delay: 500}) 
+sr.reveal('.home__electric', {delay: 600}) 
+sr.reveal('.home__img', {delay: 800})   
